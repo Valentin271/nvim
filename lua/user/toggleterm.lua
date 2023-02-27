9615f7bd30e1ee583cebe31da2f51078d708254f
Valentin271 <valentin.d77@free.fr>
2023-02-27 17:53:36 +0100

    toggleterm horizontal display

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 toggleterm.setup {
-  size = 20,
+  size = 15,
   open_mapping = [[<c-\>]],
   hide_numbers = true,
   shade_terminals = true,
@@ -12,12 +12,12 @@ toggleterm.setup {
   start_in_insert = true,
   insert_mappings = true,
   persist_size = true,
-  direction = "float",
+  direction = "horizontal",
   close_on_exit = true,
   shell = vim.o.shell,
-  float_opts = {
-    border = "curved",
-  },
+  winbar = {
+    enabled = true
+  }
 }
 
 function _G.set_terminal_keymaps()
