6da0f3ece655a3c1289661c99d4dee7e29c48243
Valentin271 <valentin.d77@free.fr>
2025-03-23 12:42:15 +0100

    feat(nvim-tree): only create inside folder if it's open

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -13,6 +13,20 @@ local function define_mappings(bufnr)
   vim.keymap.set('n', '<CR>', api.node.open.edit, opts)
   vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts)
   vim.keymap.set('n', 'v', api.node.open.vertical, opts)
+
+  -- Create inside folder only if it's open, else create inside parent folder
+  local create_callback = function(param)
+    local node = api.tree.get_node_under_cursor()
+
+    if vim.fn.isdirectory(node.absolute_path) == 1 and not node.open then
+      api.fs.create(node.parent)
+    else
+      api.fs.create(node)
+    end
+
+  end
+
+  vim.keymap.set('n', 'a', create_callback, opts)
 end
 
 nvim_tree.setup {
@@ -20,7 +34,6 @@ nvim_tree.setup {
     enable = true,
     update_cwd = true,
   },
-  select_prompts = true, -- renders nicely with dressing.nvim
   hijack_cursor = true,
   renderer = {
     root_folder_modifier = ":t",
