ff4d628d43215c21663d6e6cff4f06671caa0e37
Valentin271 <valentin.d77@free.fr>
2026-04-06 16:09:40 +0200

    fix: trigger treesitter on file load

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,15 +1,6 @@
-local status_ok, treesitter = pcall(require, "nvim-treesitter")
-if not status_ok then
-  return
-end
-
-treesitter.setup({
-  init = function()
-    vim.api.nvim_create_autocmd('FileType', {
-      callback = function()
-        pcall(vim.treesitter.start)
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end
-    })
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    pcall(vim.treesitter.start)
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end
 })
