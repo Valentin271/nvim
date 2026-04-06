ab70ab9842a2c98655f80cba21d7f837c6eb6b7f
Valentin271 <valentin.d77@free.fr>
2026-04-06 14:17:02 +0200

    refactor: use the new vim.diagnostic to configure signs

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,3 +1,25 @@
 require "user.lsp.mason"
-require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
+
+vim.diagnostic.config({
+	virtual_text = false, -- disable virtual text
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "󰌵",
+		},
+	},
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = true,
+		style = "minimal",
+		border = "rounded",
+		source = true,
+		header = "",
+		prefix = "",
+	},
+})
